1cbfb6dc87c6fd7084fd3a897471cb44a0a0e8cd
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-06-30 02:05:05 +0200

    Adjust minimum window width

--- InternalInspect/InternalTestApp.swift
+++ InternalInspect/InternalTestApp.swift
@@ -1,5 +1,18 @@
 import SwiftUI
 
+@main
+struct InternalInspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            Scratch()
+            ContentView()
+                .environment(globalState)
+                .frame(minWidth: 300)
+        }
+        .windowResizability(.contentSize)
+    }
+}
+
 public func print(_ items: String..., filePath: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
     let fileName = URL(fileURLWithPath: filePath).lastPathComponent
     let prefix = "\(fileName):\(line)::\(function)"
@@ -12,19 +25,6 @@ public func print(_ items: String..., filePath: String = #file, function : Strin
     }
 }
 
-@main
-struct InternalInspectApp: App {
-    init() {
-    }
-    
-    var body: some Scene {
-        WindowGroup {
-//            Scratch()
-            ContentView()
-                .environment(globalState)
-        }
-    }
-}
 
 extension Bundle {
     var releaseVersionNumber: String? {
